8588f35b74e6fe9ced323ec2a8508e5915c8b535
Shashwat Suri <shashwatsuri98@gmail.com>
2023-11-09 20:56:52 -0800

    commented out marching cubes, added files

--- point_extraction.sh
+++ point_extraction.sh
@@ -10,7 +10,7 @@ declare -a subjects=(
     'S8'
     'S9'
     'S11'
-)
+ )
 logbase='logs'
 outputbase='extracted_points'
 dataset='h36m_zju'
@@ -21,8 +21,8 @@ for index in "${!subjects[@]}"; do
 
     # Step 1.: train DANBO for 10k iterations, this should take about 30 minutes on RTX 3080.
     python train.py --config-name danbo_vof basedir=${logbase} expname=${expname} dataset=${dataset} dataset.subject=${subject} num_workers=16 iters=10000 
-
+    print("DONE")
     # Step 2.: run marching cubes to extract the point cloud.
-    python -m tools.extract_points --pretrained_path ${logbase}/${expname} --ckpt 10000 --threshold 20.0 --output_name ${outputbase}/anchor_pts_${subject}.th
+    # python -m tools.extract_points --pretrained_path ${logbase}/${expname} --ckpt 10000 --threshold 20.0 --output_name ${outputbase}/anchor_pts_${subject}.th
 done
 
